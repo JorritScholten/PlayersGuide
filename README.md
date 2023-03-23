c453333d281c247fa50c2602eb73448d057b00f7
Jorrit Scholten <jorrit.scholten@hotmail.com>
2023-03-23 14:54:03 +0100

    Starting with Day 23, modified README.md

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Teaching myself Java programming by following the steps in the [player's guide](players_guide_java.md).
 
-**Current task:** [Day 22](/players_guide_java.md#day-22-challenge-arrow-factories-100-xp)
+**Current task:** [Day 23](/players_guide_java.md#day-23-challenge-the-point-75-xp)
--- src/PlayersGuide.java
+++ src/PlayersGuide.java
@@ -1,36 +1,5 @@
-import item.arrow.Arrow;
-import item.arrow.ArrowHead;
-import item.arrow.Fletching;
-import util.TerminalInteraction;
-
 public class PlayersGuide {
     public static void main(String[] args) {
-        System.out.println("Starting up Player's Guide, Day 22.");
-        final String[] arrowOptions = {"Elite Arrow", "Marksman Arrow", "Beginner Arrow", "Custom Arrow"};
-        int choice = TerminalInteraction.chooseFromArray("Please choose which type of arrow to construct.", arrowOptions);
-
-        Arrow arrow;
-        switch (choice) {
-            case 0:
-                arrow = Arrow.createEliteArrow();
-                break;
-            case 1:
-                arrow = Arrow.createMarksmanArrow();
-                break;
-            case 2:
-                arrow = Arrow.createBeginnerArrow();
-                break;
-            case 3:
-                ArrowHead arrowHead = TerminalInteraction.chooseFromEnum("Arrowhead options:", ArrowHead.values());
-                Fletching fletching = TerminalInteraction.chooseFromEnum("Choice of fletching:", Fletching.values());
-                int length = TerminalInteraction.chooseIntInRange(60, 100);
-                arrow = new Arrow(length, arrowHead, fletching);
-                break;
-            default:
-                return;
-        }
-
-        System.out.println("Constructed arrow: " + arrow);
-        System.out.printf("The arrow costs %.2f gold pieces.\n", arrow.getValue());
+        System.out.println("Starting up Player's Guide, Day 23.");
     }
 }
