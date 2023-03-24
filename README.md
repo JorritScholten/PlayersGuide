576a088765bdd36d2af95cbab98390dda7bff015
Jorrit Scholten <jorrit.scholten@hotmail.com>
2023-03-24 13:04:52 +0100

    starting work on day 25, modified README.md

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Teaching myself Java programming by following the steps in the [player's guide](players_guide_java.md).
 
-**Current task:** [Day 24](/players_guide_java.md#day-24-challenge-the-color-100-xp)
+**Current task:** [Day 25](/players_guide_java.md#day-25-challenge-the-card-100-xp)
--- src/PlayersGuide.java
+++ src/PlayersGuide.java
@@ -1,9 +1,5 @@
-import concept.Color;
-
 public class PlayersGuide {
     public static void main(String[] args) {
-        System.out.println("Starting up Player's Guide, Day 24.");
-        System.out.println(new Color(0, 10, 240));
-        System.out.println(Color.createRed());
+        System.out.println("Starting up Player's Guide, Day 25.");
     }
 }
