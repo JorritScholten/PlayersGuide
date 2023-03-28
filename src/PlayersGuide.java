229890e7c929bbe8717b3c0eb4df541f4f43545e
Jorrit Scholten <jorrit.scholten@hotmail.com>
2023-03-28 12:36:41 +0200

    Completed day 26
    Implemented user interaction via TerminalInteraction.chooseFromArray() function. Maybe look into seeing if a dropdown menu is feasible in the terminal.

--- src/PlayersGuide.java
+++ src/PlayersGuide.java
@@ -7,15 +7,34 @@ public class PlayersGuide {
         System.out.println("Starting up Player's Guide, Day 26.");
         int code = TerminalInteraction.askForInt("Choose pass code for new door:");
         Door door = new Door(code, true);
-        door.open();
-        door.lock();
-        door.close();
-        door.lock();
-        System.out.println("Setting new code to 1234");
-        door.reKey(code, 1234);
-        do {
-            int unlockCode = TerminalInteraction.askForInt("Enter pass code to unlock door:");
-            door.unlock(unlockCode);
-        } while (door.getState() == DoorState.LOCKED);
+
+        String[] choices = {"Open", "Close", "Lock", "Unlock", "Change pass code", "Exit program"};
+        while (true) {
+            int choice = TerminalInteraction.chooseFromArray("Choose what to do:", choices);
+            switch (choice) {
+                case 0:
+                    door.open();
+                    break;
+                case 1:
+                    door.close();
+                    break;
+                case 2:
+                    door.lock();
+                    break;
+                case 3:
+                    do {
+                        int unlockCode = TerminalInteraction.askForInt("Enter pass code to unlock door:");
+                        door.unlock(unlockCode);
+                    } while (door.getState() == DoorState.LOCKED);
+                    break;
+                case 4:
+                    int oldCode = TerminalInteraction.askForInt("Enter current pass code:");
+                    int newCode = TerminalInteraction.askForInt("Enter new pass code:");
+                    door.reKey(oldCode, newCode);
+                    break;
+                default:
+                    return;
+            }
+        }
     }
 }
