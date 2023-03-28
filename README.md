b28bcd464bafa486b1838c6ecd0138f974288fd7
Jorrit Scholten <jorrit.scholten@hotmail.com>
2023-03-28 12:54:45 +0200

    Starting work on Day 27

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Teaching myself Java programming by following the steps in the [player's guide](players_guide_java.md).
 
-**Current task:** [Day 26](/players_guide_java.md#day-26-challenge-the-locked-door-100-xp)
+**Current task:** [Day 27](/players_guide_java.md#day-27-challenge-the-password-validator-100-xp)
--- src/PlayersGuide.java
+++ src/PlayersGuide.java
@@ -1,40 +1,5 @@
-import item.door.Door;
-import item.door.DoorState;
-import util.TerminalInteraction;
-
 public class PlayersGuide {
     public static void main(String[] args) {
-        System.out.println("Starting up Player's Guide, Day 26.");
-        int code = TerminalInteraction.askForInt("Choose pass code for new door:");
-        Door door = new Door(code, true);
-
-        String[] choices = {"Open", "Close", "Lock", "Unlock", "Change pass code", "Exit program"};
-        while (true) {
-            int choice = TerminalInteraction.chooseFromArray("Choose what to do:", choices);
-            switch (choice) {
-                case 0:
-                    door.open();
-                    break;
-                case 1:
-                    door.close();
-                    break;
-                case 2:
-                    door.lock();
-                    break;
-                case 3:
-                    do {
-                        int unlockCode = TerminalInteraction.askForInt("Enter pass code to unlock door:");
-                        door.unlock(unlockCode);
-                    } while (door.getState() == DoorState.LOCKED);
-                    break;
-                case 4:
-                    int oldCode = TerminalInteraction.askForInt("Enter current pass code:");
-                    int newCode = TerminalInteraction.askForInt("Enter new pass code:");
-                    door.reKey(oldCode, newCode);
-                    break;
-                default:
-                    return;
-            }
-        }
+        System.out.println("Starting up Player's Guide, Day 27.");
     }
 }
