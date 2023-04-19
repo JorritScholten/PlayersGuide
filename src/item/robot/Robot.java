baa0c7c6658981b9bc6a266780ec4e38c501a93e
Jorrit Scholten <jorrit.scholten@hotmail.com>
2023-04-19 14:30:36 +0200

    code can now compile

---
+++ src/item/robot/Robot.java
@@ -0,0 +1,18 @@
+package item.robot;
+
+public class Robot {
+    private int x;
+
+    private int y;
+
+    private boolean isPowered;
+
+    private RobotCommand[] commands = new RobotCommand[3];
+
+    public void run() {
+        for (RobotCommand command : commands) {
+            command.run(this);
+            System.out.printf("[%d %d %b]\n", x, y, isPowered);
+        }
+    }
+}
---
+++ src/item/robot/RobotCommand.java
@@ -0,0 +1,7 @@
+package item.robot;
+
+public class RobotCommand {
+    public void run(Robot robot) {
+
+    }
+}
