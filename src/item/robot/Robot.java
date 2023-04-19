1f6c853e24d8daef3b97b2d64d006a8e5a75f9e8
Jorrit Scholten <jorrit.scholten@hotmail.com>
2023-04-19 15:31:45 +0200

    Completed day 34

--- src/item/robot/Robot.java
+++ src/item/robot/Robot.java
@@ -9,7 +9,14 @@ public class Robot {
 
     private RobotCommand[] commands = new RobotCommand[3];
 
+    public void setCommands(RobotCommand command1, RobotCommand command2, RobotCommand command3) {
+        commands[0] = command1;
+        commands[1] = command2;
+        commands[2] = command3;
+    }
+
     public void run() {
+        System.out.printf("[%d %d %b]\n", x, y, isPowered);
         for (RobotCommand command : commands) {
             command.run(this);
             System.out.printf("[%d %d %b]\n", x, y, isPowered);
