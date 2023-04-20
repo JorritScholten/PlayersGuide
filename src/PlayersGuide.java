35c1a31ed230f810dcdacc985ed5a25ac1c67bfa
Jorrit Scholten <jorrit.scholten@hotmail.com>
2023-04-20 11:57:20 +0200

    Cleaned up day 36 according to feedback

--- src/PlayersGuide.java
+++ src/PlayersGuide.java
@@ -3,22 +3,36 @@ import concept.RoomCoordinate;
 public class PlayersGuide {
     public static void main(String[] args) {
         System.out.println("Starting up Player's Guide, Day 36.");
+
+        System.out.println("\ntesting coordinate creation and print information:");
         var origin = new RoomCoordinate(0, 0, "origin");
+        System.out.println("Created " + origin);
         var coordinate1 = new RoomCoordinate(1, 2, "room 1");
+        System.out.println("Created " + coordinate1);
         var coordinate2 = new RoomCoordinate(0, 1, "room 2");
+        System.out.println("Created " + coordinate2);
         var coordinate3 = new RoomCoordinate(1, 1, "room 3");
+        System.out.println("Created " + coordinate3);
+
+        System.out.println("\ntesting coordinate adjacency and print results:");
         compareCoordinates(origin, coordinate1);
         compareCoordinates(origin, coordinate2);
         compareCoordinates(origin, coordinate3);
         compareCoordinates(coordinate2, coordinate3);
+
+        System.out.println("\ntest creation of duplicate coordinate, " +
+                "program should raise Exception and stop before printing information:");
         var newOrigin = new RoomCoordinate(0, 0, "new origin");
+        System.out.println("Created " + newOrigin);
     }
 
     private static void compareCoordinates(RoomCoordinate coordinate1, RoomCoordinate coordinate2) {
         if (coordinate1.isAdjacent(coordinate2)) {
-            System.out.printf("%s and %s are adjacent.\n", coordinate1.getName(), coordinate2.getName());
+            System.out.printf("\033[32m%s and %s are adjacent.\033[0m\n",
+                    coordinate1.getName(), coordinate2.getName());
         } else {
-            System.out.printf("\033[31m%s and %s are not adjacent.\033[0m\n", coordinate1.getName(), coordinate2.getName());
+            System.out.printf("\033[31m%s and %s are not adjacent.\033[0m\n",
+                    coordinate1.getName(), coordinate2.getName());
         }
     }
 }
--- src/concept/RoomCoordinate.java
+++ src/concept/RoomCoordinate.java
@@ -4,19 +4,35 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class RoomCoordinate {
-    private static Set<Coordinate> coordinates = new HashSet<>();
+    /** Keeping track of all created coordinates using a static Set ensures coordinates are immutable. */
+    private static final Set<Coordinate> coordinates = new HashSet<>();
+    /** Position of room. */
     private final Coordinate coordinate;
+    /** Name of room for identification purposes. */
     private final String name;
 
+    /**
+     * Create RoomCoordinate, a named room with a unique location.
+     * @param x    Location of room on X-axis.
+     * @param y    Location of room on Y-axis.
+     * @param name Name of room for identification purposes.
+     * @throws IllegalArgumentException when a room already exists at the specified location.
+     */
     public RoomCoordinate(int x, int y, String name) throws IllegalArgumentException {
         coordinate = new Coordinate(x, y);
         if (!coordinates.add(coordinate)) {
-            throw new IllegalArgumentException(String.format("Coordinate (%d,%d) already in use, \"%s\" must be unique.", x, y, name));
+            throw new IllegalArgumentException(
+                    String.format("Coordinate (%d,%d) already in use, \"%s\" must be unique.", x, y, name)
+            );
         }
         this.name = name;
-        System.out.printf("Created RoomCoordinate \"%s\"(%d,%d)\n", name, x, y);
     }
 
+    /**
+     * Checks if another room is directly adjacent (not diagonal) to itself.
+     * @param otherRoomCoordinate Other room to check against.
+     * @return true if other room is adjacent.
+     */
     public boolean isAdjacent(RoomCoordinate otherRoomCoordinate) {
         var otherCoordinate = otherRoomCoordinate.getCoordinate();
         return ((coordinate.x == (otherCoordinate.x - 1)) && (coordinate.y == otherCoordinate.y)) ||
@@ -33,6 +49,16 @@ public class RoomCoordinate {
         return coordinate;
     }
 
+    @Override
+    public String toString() {
+        return String.format("RoomCoordinate \"%s\"(%d,%d)", name, coordinate.x, coordinate.y);
+    }
+
+    /**
+     * Type container for 2D coordinate system.
+     * @param x position on X-axis.
+     * @param y position on Y-axis.
+     */
     public record Coordinate(int x, int y) {
     }
 }
