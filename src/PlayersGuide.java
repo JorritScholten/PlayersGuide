75604c6d969e02617127b7402bea3dab26319dc7
Jorrit Scholten <jorrit.scholten@hotmail.com>
2023-04-25 10:41:17 +0200

    Completed day 37

--- src/PlayersGuide.java
+++ src/PlayersGuide.java
@@ -1,5 +1,15 @@
+import item.sword.Gemstone;
+import item.sword.Material;
+import item.sword.Sword;
+
 public class PlayersGuide {
     public static void main(String[] args) {
         System.out.println("Starting up Player's Guide, Day 37.");
+        Sword defaultSword = Sword.createDefaultSword();
+        System.out.println("Created default sword: " + defaultSword);
+        Sword modifiedSword = defaultSword.createModifiedCopy(Material.BRONZE);
+        System.out.println("Created modified sword: " + modifiedSword);
+        Sword modifiedSword2 = modifiedSword.createModifiedCopy(Gemstone.DIAMOND);
+        System.out.println("Modified sword further: " + modifiedSword2);
     }
 }
--- src/item/sword/Sword.java
+++ src/item/sword/Sword.java
@@ -4,35 +4,49 @@ import concept.HasDescription;
 import item.InventoryItem;
 
 public class Sword extends InventoryItem implements HasDescription {
-    private Material material;
-    private Gemstone gemstone;
-    private int length;
-    private int crossguardWidth;
+    private final Material material;
+    private final Gemstone gemstone;
+    private final int length;
+    private final int crossguardWidth;
 
-    public Sword(double weight, double volume) {
+    public Sword(double weight, double volume, int length, int crossguardWidth, Material material, Gemstone gemstone) {
         super(weight, volume);
-    }
-
-    public Sword(int length, int crossguardWidth) {
-        super(5, 3);
         this.length = length;
         this.crossguardWidth = crossguardWidth;
-        material = Material.IRON;
-        gemstone = null;
+        this.material = material;
+        this.gemstone = gemstone;
+    }
+
+    static public Sword createDefaultSword() {
+        double DEFAULT_VOLUME = 3;
+        double DEFAULT_WEIGHT = 5;
+        int DEFAULT_CROSSGUARD_WIDTH = 20;
+        int DEFAULT_LENGTH = 130;
+        return new Sword(DEFAULT_WEIGHT, DEFAULT_VOLUME, DEFAULT_LENGTH, DEFAULT_CROSSGUARD_WIDTH, Material.IRON, null);
+    }
+
+    public Sword createModifiedCopy(Material material) {
+        return new Sword(getWeight(), getVolume(), length, crossguardWidth, material, gemstone);
+    }
+
+    public Sword createModifiedCopy(Gemstone gemstone) {
+        return new Sword(getWeight(), getVolume(), length, crossguardWidth, material, gemstone);
     }
 
     @Override
     public String toString() {
-        return "Sword";
+        return getShortDescription();
     }
 
     @Override
     public String getDescription() {
-        return null;
+        return String.format("%s sword with %s gem inset, %d cm long and a crossguard %d cm wide.",
+                material.getDescription(), gemstone == null ? "no" : gemstone.getShortDescription(), length, crossguardWidth);
     }
 
     @Override
     public String getShortDescription() {
-        return null;
+        return String.format("%s sword, %d cm long, %d cm crossguard width%s",
+                material.getDescription(), length, crossguardWidth, gemstone == null ? "" : ", " + gemstone.getDescription());
     }
 }
